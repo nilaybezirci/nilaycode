df877227f26aa1c4c651d6a3c92a6f3bba3de76b
nilaybezirci <mnilaybezirci@gmail.com>
2021-05-25 17:15:07 +0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href=" css/style.css">
+
+  <title>b201210035 M.NilayBezirci</title>
+  <script src="https://kit.fontawesome.com/94c2f0f80e.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+  <!--Menü Başlangıç-->
+
+  <a class="navbar-brand" href="#">b201210035 || <I>Nilay Bezirci</I> </a>
+  <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          -->
+
+
+  <!--Menü Bitiş -->
+  <header>
+    <div class=" text-light  ">
+      <section class=" p-5  ">
+
+        <div class="container">
+          <h1 class="font-weight-bold yazi">Giriş</h1>
+
+          <hr class="ayrac"><i class="far fa-moon isari"></i>
+          
+
+        </div>
+    </div>
+  </header>
+  <main>
+            <div >
+
+              <form action="Loginm.php" method="POST">
+                <div class="form-group ">
+                  <label for="email">Email</label>
+                  <input type="text" name="email" class="form-control" placeholder="Email " required="required">
+                </div>
+                <div class="form-group ">
+                  <label for="password">Şifre</label>
+                  <input type="password" name="password" class="form-control" placeholder="Şifre" required="required">
+                </div>
+                <button class="btn btn-primary " type="submit">Gönder</button>
+                <br><br><br>
+              </form>
+            </div>
+          </main>
+
+
+
+
+  <!-- Login başlangıç  -->
+  <!-- <section class=" p-5 text-center login">
+
+     <div class="container logins">
+      <h1 class="font-weight-bold">Login </h1>
+      <hr class="ayrac"><i class="far fa-moon iyeşil"></i>
+      <form>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Email adres</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          <div id="emailHelp" class="form-text">e-mail adresinizi asla başkalarıyla paylaşmayacağız</div>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Şifre</label>
+          <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+
+        <button type="girisyap" class="btn btn-primary">Giriş yap</button>
+      </form>
+
+
+    </div> 
+ 
+  </section> -->
+
+
+  <!-- Login bitiş -->
+
+
+
+
+
+
+
+  <div class="footer">
+    <footer class="py-5  text-white text-center">
+      Web-Teknolojileri-Projesi © Nilay Bezirci
+    </footer>
+  </div>
+
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+
+</body>
